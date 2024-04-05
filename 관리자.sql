1fc8ef2321d497267b6ae962e96c7d26db22a7d4
김도연 <dozagirugi@gmail.com>
2024-04-05 12:36:35 +0900

    Add files via upload

---
+++ 관리자.sql
@@ -0,0 +1,8 @@
+-- 이것이 주석이다 '--'하거나 ctrl + / 누르기
+
+select sysdate from dual;
+-- 날짜 출력하기
+
+@C:\oraclexe\app\oracle\product\11.2.0\server\rdbms\admin\scott.sql
+alter user scott identified by 1234;
+-- 스콧 계정 만들기
